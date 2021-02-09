871d074a82a63e44931913a33c8d6921ed25a42a
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-08 16:16:56 -0800

    in progress: adding event listener to photourl input #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 /* global data */
 /* exported data */
+var $photoUrlInput = document.querySelector('#photoUpload');
+var $image = document.querySelector('img');
+$photoUrlInput.addEventListener('input', function (event) {
+
+});
