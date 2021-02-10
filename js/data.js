35b4052dafad219ee10021435a63041da735a5be
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-09 17:21:03 -0800

    moved data js lines 10-30 to main js #1

--- js/data.js
+++ js/data.js
@@ -6,35 +6,3 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-
-var $imageURL = document.querySelector('input[name="imageUpload"]');
-var $textArea = document.querySelector('textarea[name="notes"]');
-var $title = document.querySelector('input[name="title"]');
-var $img = document.querySelector('img');
-
-$imageURL.addEventListener('input', function (event) {
-  $img.setAttribute('src', $imageURL.value);
-
-});
-
-var $entryForm = document.querySelector('div.entry-form');
-$entryForm.addEventListener('submit', function (event) {
-  var inputVals = {};
-  inputVals.imageURL = $imageURL.value;
-  inputVals.title = $title.value;
-  inputVals.textarea = $textArea.value;
-  inputVals.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(inputVals);
-  $entryForm.reset();
-});
-
-var previousDataJSON = localStorage.getItem('create-an-entry');
-if (previousDataJSON != null) {
-  data = JSON.parse(previousDataJSON);
-}
-
-window.addEventListener('beforeunload', function (event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('create-an-entry', dataJSON);
-});
