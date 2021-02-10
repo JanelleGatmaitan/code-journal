98e21e56bfe73eea37bde52b9e145b7d6583ea1f
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-09 19:44:55 -0800

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,24 @@ $entryForm.addEventListener('submit', function (event) {
   data.entries.unshift(inputVals);
   $entryForm.reset();
 });
+
+function renderEntry(entry) {
+  var divRow = document.createElement('div');
+  var rowAttribute = divRow.setAttribute('class', 'row');
+  var divColHalf = document.createElement('div');
+  var ColHalfAttribute = divColHalf.setAttribute('class', 'column-half');
+  divRow.appendChild(divColHalf);
+  var entryImg = document.createElement('img');
+  var entryImgAttribute = entryImg.setAttribute('src', entry.imageURL);
+  divColHalf.appendChild(entryImg);
+  var secondDivColHalf = document.createElement('div');
+  var secondColHalfAttribute = secondDivColHalf.setAttribute('class', 'column-half');
+  var entryH2 = document.createElement('h2');
+  var h2Text = document.createTextNode(entry.title);
+  var note = document.createElement('p');
+  var noteText = document.createTextNode(entry.textarea);
+  secondDivColHalf.appendChild(entryH2);
+  secondDivColHalf.appendChild(noteText);
+  divRow.appendChild(secondDivColHalf);
+  return divRow;
+}
